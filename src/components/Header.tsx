998e94112086c3d13d948266f55a036eefa41868
Jonathan Easterman <jonathaneasterman@gmail.com>
2022-10-22 12:14:28 +0100

    Hide nav links on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,17 +15,15 @@ const Header: FC = () => {
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="https://cryptonet.org/projects/project-medusa-scalable-threshold-network-on-chain">
               <span className="sr-only">Medusa</span>
-              <img className="h-8 w-auto sm:h-16" src="/logo.png" alt="" />
+              <img className="h-12 w-auto sm:h-16" src="/logo.png" alt="" />
             </a>
           </div>
-          <nav className="space-x-10 md:flex">
+          <nav className="space-x-10 hidden sm:flex">
             <a href="https://goerli-faucet.pk910.de/" target="_blank" className="text-base font-medium text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400" rel="noreferrer">Faucet</a>
             <a href="https://chainlist.org/" target="_blank" className="text-base font-medium text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400" rel="noreferrer">Add Arbitrum Goerli</a>
           </nav>
           <div className="items-center justify-end flex flex-1 lg:w-0 space-x-3">
             <ThemeSwitcher />
-
-
             {isConnected ? <Signin /> : <ConnectWallet />}
           </div>
         </div>
