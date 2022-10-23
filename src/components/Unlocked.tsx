df49c770834372fd08922c24398dad4b6cf369b5
Jonathan Easterman <jonathaneasterman@gmail.com>
2022-10-23 13:20:33 +0100

    Truncate overflowing text with '...'

--- src/components/Unlocked.tsx
+++ src/components/Unlocked.tsx
@@ -95,7 +95,7 @@ const Unlocked: FC<Sale> = ({ buyer, seller, requestId, cipherId }) => {
 
   return (
     <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{listing.name}</h5>
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white truncate">{listing.name}</h5>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{listing.description}</p>
       <p className="mb-3">{BigNumber.from(0).eq(listing.price) ? "Free" : `${formatEther(listing.price)} ETH`} </p>
       <a href={downloadLink} download={listing.name} className="inline-flex items-center text-blue-600 hover:underline">
