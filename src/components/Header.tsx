f027c6048514029c4b0e9dfd7dfe606a25cecee7
Jonathan Easterman <jonathaneasterman@gmail.com>
2022-10-23 12:14:01 +0100

    Change logo link to https://medusanet.xyz

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ const Header: FC = () => {
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between border-b-2 border-black dark:border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="https://cryptonet.org/projects/project-medusa-scalable-threshold-network-on-chain">
+            <a href="https://medusanet.xyz">
               <span className="sr-only">Medusa</span>
               <img className="h-12 w-auto sm:h-16" src="/logo.png" alt="" />
             </a>
