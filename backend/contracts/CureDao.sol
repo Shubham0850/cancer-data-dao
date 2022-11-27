67bd77e72de4d6c0da67e09560e546857a6d3c3e
Harendra Shakya <harendrashakya125@gmail.com>
2022-11-27 19:08:26 +0530

    add: whitelisted address logic

--- backend/contracts/CureDao.sol
+++ backend/contracts/CureDao.sol
@@ -7,23 +7,41 @@ contract CureDao {
     address private constant TREASURY = 0xC3A3362DC30588a027767063459dC533Dc4A421a;
     address private constant MEDUSA = 0xDbf5B82C9b3Cd8291878b4d355368ab6e32b9A14;
 
+    mapping(address => bool) private whitelistedAddresses;
+
+    modifier isWhitelisted() {
+        require(whitelistedAddresses[msg.sender], "CureDao: Address not whitelisted");
+        _;
+    }
+
+    constructor() {
+        whitelistedAddresses[msg.sender] = true;
+    }
+
     function addData(
         uint256[] memory cipher,
         string memory name,
         string memory description,
         uint256 price,
         string memory uri
-    ) external {
+    ) external isWhitelisted {
         IMedusa(MEDUSA).createListing(cipher, name, description, price, uri);
     }
 
     function buyData(uint256 cipherId, uint256[] memory buyerPublicKey) external {
         IMedusa(MEDUSA).buyListing(cipherId, buyerPublicKey);
-        withdrawPayments();
     }
 
-    function withdrawPayments() public {
+    function withdrawPayments() external {
         IMedusa(MEDUSA).withdrawPayments(TREASURY);
+    }
+
+    function addWhitelistAddress(address newAddress) external isWhitelisted {
+        whitelistedAddresses[newAddress] = true;
+    }
+
+    function getIsWhiteListed(address _address) external view returns (bool _isWhitelisted) {
+        _isWhitelisted = whitelistedAddresses[_address];
     }
 
     function getListing(uint256 cipherId)
